6e7cce1412f31deae9397d1b7deb566965222848
jinhyo <sosilion@gmail.com>
2025-07-11 06:15:47 +0900

    feat: 북마크 삭제기능 리스트 인풋으로 변경

--- src/application/bookmark/Bookmark.service.ts
+++ src/application/bookmark/Bookmark.service.ts
@@ -40,21 +40,21 @@ export class BookmarkService {
     return this.bookmarkRepository.saveOne(bookmark);
   }
 
-  async deleteBookmark(
-    userId: number,
-    bookmarkId: number,
-  ): Promise<BookmarkOrmEntity> {
-    const bookmark = await this.bookmarkRepository.findOneByIdAndUserId(
-      bookmarkId,
-      userId,
+  async deleteBookmark(bookmarkIds: number[]): Promise<BookmarkOrmEntity[]> {
+    console.log(
+      '🚀 ~ BookmarkService ~ deleteBookmark ~ bookmarkIds:',
+      bookmarkIds,
     );
+    const bookmarks = await this.bookmarkRepository.findManyByIds(bookmarkIds);
 
-    if (!bookmark) {
+    if (bookmarks.length === 0) {
       throw new NotFoundException('Bookmark not found');
     }
 
-    await this.bookmarkRepository.removeOneById(bookmarkId);
-    return bookmark;
+    for (const bookmark of bookmarks) {
+      await this.bookmarkRepository.removeOneById(bookmark.id);
+    }
+    return bookmarks;
   }
 
   async getBookmarkList(userId: number): Promise<BookmarkOrmEntity[]> {
--- src/controller/bookmark/rest/Bookmark.controller.ts
+++ src/controller/bookmark/rest/Bookmark.controller.ts
@@ -1,9 +1,8 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Inject,
   Param,
   ParseIntPipe,
@@ -12,11 +11,8 @@ import {
 } from '@nestjs/common';
 import { LoginUser } from 'src/base/decorator/LoginUser.decorator';
 import { LoginGuard } from '@base/guard/Login.token.guard';
-import { BookmarkResDto } from '../../abc/dto/Bookmark.res.dto';
 import { BookmarkService } from '@application/bookmark/Bookmark.service';
 import { BookmarkOrmEntity } from '@entity/Bookmark.orm.entity';
-import { PlaceOrmEntity } from '@entity/Place.orm.entity';
-import { PlaceInfoResDto } from '@controller/abc/dto/PlaceInfo.res.dto';
 
 @Controller('bookmarks')
 @UseGuards(LoginGuard)
@@ -34,12 +30,11 @@ export class BookmarkController {
     return await this.bookmarkService.addBookmark(userId, placeId);
   }
 
-  @Delete('/:bookmarkId')
+  @Delete()
   async deleteBookmark(
-    @Param('bookmarkId', ParseIntPipe) bookmarkId: number,
-    @LoginUser() userId: number,
-  ): Promise<BookmarkOrmEntity> {
-    return await this.bookmarkService.deleteBookmark(userId, bookmarkId);
+    @Body('ids') ids: number[],
+  ): Promise<BookmarkOrmEntity[]> {
+    return await this.bookmarkService.deleteBookmark(ids);
   }
 
   @Get()
