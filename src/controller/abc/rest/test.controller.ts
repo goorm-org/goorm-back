95bc836a9938a94e9c6dcb9ea5ff42653484e3be
jinhyo <sosilion@gmail.com>
2025-07-11 11:24:03 +0900

    feat: port 변경

---
+++ src/controller/abc/rest/test.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { GetAbcByIdHandler } from '@application/abc/handler/GetAbcById.handler';
+import { CreateAbcHandler } from '@application/abc/handler/CreateAbc.handler';
+import { AbcResDto } from '../dto/Abc.res.dto';
+import { CreateAbcReqDto } from '../dto/CreateAbc.req.dto';
+
+@Controller()
+export class AbcController {
+  constructor(
+    private readonly getAbcByHandler: GetAbcByIdHandler,
+    private readonly createAbcHandler: CreateAbcHandler,
+  ) {}
+
+  @Get()
+  findOne(@Param('id') id: string) {
+    return this.getAbcByHandler.handle(+id);
+  }
+
+  @Post()
+  async createAbc(@Body() createAbcDto: CreateAbcReqDto) {
+    const abc = await this.createAbcHandler.handle(
+      createAbcDto.name,
+      createAbcDto.age,
+      createAbcDto.isActive,
+    );
+
+    return new AbcResDto(abc);
+  }
+}
